4cb88e818f605d22d2bdc576e6f032b750fd1623
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-04-20 16:57:17 +0200

    feat: Refactor product options and update styles for better responsiveness

---
+++ src/scripts/shopOptions.js
@@ -0,0 +1,70 @@
+
+
+//Open and close the menu
+document.addEventListener("DOMContentLoaded", () => {
+    const Pedir = document.getElementsByClassName("openPEDIR");
+    const Menu = document.getElementById("Buy");
+    const Buttons = document.getElementById("Options");
+    const X = document.getElementById("x");
+
+    Pedir[0].addEventListener("click", (event) => {
+        Menu.classList.remove("none");
+        Menu.classList.add("Buy");
+        Buttons.classList.remove("Options");
+        Buttons.classList.add("none");
+    });
+
+    X.addEventListener("click", (event) => {
+        Menu.classList.add("BuyAnimation");
+        setTimeout(() => {
+            Menu.classList.remove("BuyAnimation"); 
+            Menu.classList.remove("Buy");
+            Menu.classList.add("none");
+            Buttons.classList.remove("none");
+            Buttons.classList.add("Options");
+        }, 200);
+
+    });
+
+});
+
+
+//Open and close the mobile menu
+document.addEventListener("DOMContentLoaded", () => {
+    const article = document.getElementsByClassName("product");
+    const options = document.getElementById("Options");
+    let clicked = false;
+
+    article[0].addEventListener("click", (event) => {
+        if (clicked) {
+            options.classList.add("DesktopOnly");
+            clicked = false;
+        } else {
+            options.classList.remove("DesktopOnly");
+            clicked = true;
+        }
+    });
+
+});
+
+//Add or increment the quantity of the product
+document.addEventListener("DOMContentLoaded", () => {
+    const add = document.getElementById("+");
+    const remove = document.getElementById("-");
+    const quantity = document.getElementById("amountofproduct");
+    let count = 1;
+    quantity.value = count;
+
+
+    add.addEventListener("click", (event) => {
+        count++;
+        quantity.value = count;
+    });
+
+    remove.addEventListener("click", (event) => {
+        if (count > 1) {
+            count--;
+            quantity.value = count;
+        }
+    });
+});
